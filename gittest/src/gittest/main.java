d77ddc5fa3254abb6de09fa20ab35e668ad8dce0
robustarJJ <tama00@naver.com>
2023-11-09 17:56:29 +0900

    뺼셈 메서드 추가입니다

--- gittest/src/gittest/main.java
+++ gittest/src/gittest/main.java
@@ -3,11 +3,23 @@ package gittest;
 public class main {
 
 	public static void main(String[] args) {
+		
+		
+		
+		
 		System.out.println("init");
+		
+		
+		
+		
 	}
 	
 	public void plus(int a , int b) {
-		System.err.println(a+b);
+		System.out.println(a+b);
+	}
+	
+	public void minus(int a , int b) {
+		System.out.println(a-b);
 	}
 	
 
