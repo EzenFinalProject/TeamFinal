6edd6c3357d02de5cd2aa6e0286c101003373b2b
robustarJJ <tama00@naver.com>
2023-11-09 20:40:58 +0900

    병합충돌 해결

--- gittest/src/gittest/Jang_wooho.java
+++ gittest/src/gittest/Jang_wooho.java
@@ -13,4 +13,28 @@ public class Jang_wooho {
 		System.out.println("Jwh");
 	}
 	
+	public void test1() {
+		
+	}
+	public void test2() {
+		
+	}
+	public void test3() {
+		
+	}
+	public void test4() {
+		
+	}
+	public void test5() {
+		
+	}
+	public void test6() {
+		
+	}
+	public void test7() {
+		
+	}
+	public void test8() {
+		
+	}
 }
