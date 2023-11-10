8b1320b08ae05eecfcfd3f0fc81c62fc5d6ad601
JeonHyeSeong <bc7399@naver.com>
2023-11-10 16:59:46 +0900

    testVO getter/setter 추가

--- gittest/src/gittest/TestVO.java
+++ gittest/src/gittest/TestVO.java
@@ -6,6 +6,28 @@ public class TestVO {
 
 	private String test1;
 	private String test2;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTest1() {
+		return test1;
+	}
+	public void setTest1(String test1) {
+		this.test1 = test1;
+	}
+	public String getTest2() {
+		return test2;
+	}
+	public void setTest2(String test2) {
+		this.test2 = test2;
+	}
+	
+	
 
 
 }
