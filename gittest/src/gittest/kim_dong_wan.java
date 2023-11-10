13eb3d69459da46decdf66e6f7ec51a0205b3732
robustarJJ <tama00@naver.com>
2023-11-10 19:51:42 +0900

    부가기능 생성

--- gittest/src/gittest/kim_dong_wan.java
+++ gittest/src/gittest/kim_dong_wan.java
@@ -5,5 +5,23 @@ public class kim_dong_wan {
 	private String name = "김동완";
 	private String name2 = "장우호";
 	
-	
+	public void name() {
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+		System.out.println("김동완");
+	}
 }
